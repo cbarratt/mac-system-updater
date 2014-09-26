7b2415f7dcd8ee0c384916f337ef1525fbff779c
Callum Barratt <callum@barratt.me>
2014-09-26 09:44:36 +0100

    Update script

--- update.rb
+++ update.rb
@@ -7,13 +7,10 @@ module System
   class Update
     def perform
       system_info
-      break_output
       update_brew_packages
-      break_output
       update_oh_my_zsh
-      break_output
-      update_ruby_manager
-      break_output
+      update_rvm if File.exists? File.expand_path('~/.rvm')
+      update_rbenv if File.exists? File.expand_path('~/.rbenv')
       check_mac_store_updates
     end
 
@@ -31,6 +28,7 @@ module System
       puts '  - OSX: ' + %x{sw_vers | awk -F':\t' '{print $2}' | paste -d ' ' - - -}
       puts '  - Host: ' + %x{scutil --get ComputerName}
       puts '  - RAM: ' + %x{sysctl -n hw.memsize | awk '{print $0/1073741824" GB RAM"}'}
+      break_output
     end
 
     def check_mac_store_updates
@@ -40,30 +38,43 @@ module System
         error = stderr.read
         puts '  - No new software updates available.' if error.include?('No new software available')
       end
+      break_output
     end
 
-    def update_ruby_manager
-      if %x{~/.rvm/bin/rvm-prompt}
-        check_update_message('RVM')
-        Open3.popen3('rvm get stable') do |stdin, stdout, stderr, thread|
-          output = stdout.read
-          puts '  - RVM updated.' if output.include?('RVM reloaded')
-        end
+    def update_rvm
+      check_update_message('RVM')
+      Open3.popen3('rvm get stable') do |stdin, stdout, stderr, thread|
+        output = stdout.read
+        puts '  - RVM updated.' if output.include?('RVM reloaded')
       end
+      break_output
+    end
+
+    def update_rbenv
+      check_update_message('rbenv')
+      Open3.popen3('cd ~/.rbenv && git pull && cd cd plugins/ruby-build/ && git pull') do |stdin, stdout, stderr, thread|
+        puts '  - rbenv updated.'
+      end
+      break_output
     end
 
     def update_oh_my_zsh
-      check_update_message('Oh My Zsh')
+      if File.exists? File.expand_path('~/.oh-my-zsh')
+        check_update_message('Oh My Zsh')
 
-      Open3.popen3('env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh') do |stdin, stdout, stderr, thread|
-        output = stdout.read
-        if output.include?('Current branch master is up to date')
-          puts '  - Oh My Zsh already up to date.'
-        else
-          puts '# Updating Oh My Zsh...'
-          system 'env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh'
+        Open3.popen3('env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh') do |stdin, stdout, stderr, thread|
+          output = stdout.read
+          if output.include?('Current branch master is up to date')
+            puts '  - Oh My Zsh already up to date.'
+          else
+            puts '# Updating Oh My Zsh...'
+            system 'env ZSH=$ZSH /bin/sh $ZSH/tools/upgrade.sh'
+          end
         end
+      else
+        puts '# Oh My Zsh is not installed. Skipped.'
       end
+      break_output
     end
 
     def update_brew_packages
@@ -74,6 +85,7 @@ module System
         puts '# Updating Homebrew packages...'
         system 'brew update'
       end
+      break_output
     end
   end
 end
