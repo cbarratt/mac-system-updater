b349d2bec075135a0c3d77cf5c3a4b56b56470c0
Anthony Lombardi <Sic@server.local>
2015-02-03 22:48:56 -0500

    tweaked update.rb and had it require system.rb

--- update.rb
+++ update.rb
@@ -2,9 +2,13 @@
 APP_ROOT = File.dirname(__FILE__)
 $:.unshift(File.join(APP_ROOT, 'lib') )
 
-if Gem::Platform.local =~ /Darwin/i
 
+os = Gem::Platform.local
+
+if os.to_s =~ /[darwin]/i
+	require 'system'
   System::Update.new.perform
 else
   puts  "\tThis program requires Mac OS X to run. \n\tPlease Use a Mac OSX machine."
 end
+
