a9b5f0dc53353452d9b0e545b43b5bf8728c554f
Anthony Lombardi <Sic@server.local>
2015-02-03 22:55:23 -0500

    Use refinements for a String hack

--- lib/utility.rb
+++ lib/utility.rb
@@ -1,3 +1,13 @@
+module StringExtensions
+  refine String do
+    def undent
+      gsub(/^.{#{(slice(/^ +/) || '').length}}/, '')
+    end
+
+    alias_method :undent_________________________________________________________72, :undent
+  end
+end
+
 String.class_eval do
   def indent(count, char = ' ')
     gsub(/([^\n]*)(\n|$)/) do |match|
@@ -49,21 +59,4 @@ def run?
     puts '  - Do you want to perform this action? (y/n)'
     yes_or_no
   end
-end
-
-class String
-  def undent
-    gsub(/^.{#{(slice(/^ +/) || '').length}}/, '')
-  end
-  # eg:
-  #   if foo then <<-EOS.undent_________________________________________________________72
-  #               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-  #               eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-  #               minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-  #               ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-  #               voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur
-  #               sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-  #               mollit anim id est laborum.
-  #               EOS
-  alias_method :undent_________________________________________________________72, :undent
 end
